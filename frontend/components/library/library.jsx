01723504848a5223be6531a05ed5f6f2b445b932
Harry Zec <harryzec@gmail.com>
2020-04-02 01:34:32 -0400

    goodstuff

--- frontend/components/library/library.jsx
+++ frontend/components/library/library.jsx
@@ -3,7 +3,61 @@ import { Link } from 'react-router-dom'
 
 
 class Library extends React.Component {
+
+
   render() {
+
+    let box0;
+    let box1;
+    let box2;
+    let box3;
+    let box4;
+    let box5;
+
+    let recentplays = this.props.recentplays.slice(0, 6).map(play => {
+      
+      return(
+        <>
+          <div className='boxadjust'>
+            <div className='box'>
+              <img className='boxpic'src={play.imgUrl}/>
+            </div>
+            <Link className='boxtitle' to={`/${play.user.split(' ').join('-')}/${play.title}`}>{play.title}</Link>
+            <Link className='boxuser' to={`/${play.user.split(' ').join('-')}`}>{play.user}</Link>
+          </div>
+
+        </>
+      )
+    })
+
+    let likes = this.props.currentuser.likes.reverse().slice(0, 6).map(like=>{
+      return(
+        <>
+          <div className='boxadjust'>
+            <div className='box'>
+              <img className='boxpic'src={like.imgUrl}/>
+            </div>
+            <Link className='boxtitle' to={`/${like.user.split(' ').join('-')}/${like.title}`}>{like.title}</Link>
+            <Link className='boxuser' to={`/${like.user.split(' ').join('-')}`}>{like.user}</Link>
+          </div>
+
+        </>
+      )
+    })
+    
+    // if (this.props.recentplays.length > 6) {
+    //   let num = 6 - this.props.recentplays.length
+    //   for (i = 0; i < num; i ++) {
+    //     (box + i) += (
+    //       <>
+    //         <div className='box'>
+
+    //         </div>
+    //       </>
+    //     )
+    //   }
+    // }
+
     return(
       <>
         <div className='librarypage'>
@@ -12,6 +66,23 @@ class Library extends React.Component {
             <Link className='libraryhead'>Likes</Link>
             <Link className='libraryhead'>Playlists</Link>
             <Link className='libraryhead'>Following</Link>
+          </div>
+
+          <h2 className='recentplayshead'>Recently played</h2>
+
+          <div className='libraryrecentplays'>
+            {recentplays}
+            {box0}
+            {box1}
+            {box2}
+            {box3}
+            {box4}
+            {box5}
+          </div>
+
+          <h2 className='recentplayshead'>Likes</h2>
+          <div className='libraryrecentplays'>
+            {likes}
           </div>
 
         </div>
--- frontend/components/library/library_container.jsx
+++ frontend/components/library/library_container.jsx
@@ -1,12 +1,13 @@
 import { connect } from 'react-redux';
 import Library from './library'
 
-const mSTP = state => {
+const mSTP = state => ({
+  recentplays: Object.values(state.entities.recentplays),
+  currentuser: state.session.currentUser
+})
 
-}
+const mDTP = dispatch =>({
 
-const mDTP = dispatch =>{
-
-}
+})
 
 export default connect(mSTP, mDTP)(Library)
